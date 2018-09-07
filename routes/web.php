02f18094211e397193e46351e56b6a9233fb9c0e
fpestunov <fpestunov@gmail.com>
2018-09-07 22:06:58 +0400

    Install Laravel and add view

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $title = 'Welcome Laravel';
+    $tasks = [
+        'Go to the store',
+        'Finish my',
+        'Clean the house'
+    ];
+    return view('welcome', compact('title', 'tasks'));
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
