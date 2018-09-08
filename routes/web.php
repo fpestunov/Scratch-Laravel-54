d9c25682e2d5661f8d783f1a8289aad8bd814291
fpestunov <fpestunov@gmail.com>
2018-09-08 13:12:59 +0400

    Make Structure with Layouts

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/posts', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
+
 Route::get('/tasks', 'TaskController@index');
 Route::get('/tasks/{task}', 'TaskController@show');
 
