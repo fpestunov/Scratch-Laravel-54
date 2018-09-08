976865b5ccdad0c54b8df9ca2c4ff13015f6ffb8
fpestunov <fpestunov@gmail.com>
2018-09-08 08:27:13 +0400

    Add model and Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
 
 
 Route::get('/about', function () {
@@ -19,7 +10,10 @@ Route::get('/about', function () {
 Route::get('/tasks', function () {
     $title = 'Welcome Laravel';
 
-    $tasks = DB::table('tasks')->get();
+    // $tasks = DB::table('tasks')->get();
+    $tasks = App\Task::all();
+    // Добавляем 'use App\Task' и пишем так
+    $tasks = Task::all();
 
     // $tasks = [
     //     'Go to the store',
@@ -30,8 +24,10 @@ Route::get('/tasks', function () {
 });
 
 Route::get('/tasks/{task}', function ($id) {
-    
-    $task = DB::table('tasks')->find($id);
+
+    // $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
+
     // dd($task);
     return view('tasks.show', compact('task'));
 
